dc725f179982548fd22f713fdd86b3ebdfb2d535
Pohubi Zoltán <pohubi.zoltan@gmail.com>
2015-03-19 11:57:28 +0100

    v0.1.2.1

--- Szkeleton/src/SkeletonHelper.java
+++ Szkeleton/src/SkeletonHelper.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public abstract class SkeletonHelper {
 	static int t = 0;
@@ -27,11 +31,38 @@ public abstract class SkeletonHelper {
 	
 	public static boolean getBooleanAnswer (String s)
 	{
+		System.out.print(repeat("/t", t) + s + " (I/N): ");
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			while (true)
+			{
+				String line = br.readLine();
+				if (line == null)
+					continue;
+			
+				if ( ('I' == line.charAt(0)) || ('i' == line.charAt(0)) )
+				{	
+					br.close();
+					return true;
+				}
+				if ( ('N' == line.charAt(0)) || ('n' == line.charAt(0)) )
+				{	
+					br.close();
+					return false;
+				}
+				System.out.println("Rossz bemenet, próbáld újra!");
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
 	public static int getIntegerAnswer (String s)
 	{
+		System.out.println(s);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		return 0;
 	}
 	
