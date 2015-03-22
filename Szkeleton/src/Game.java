e1cc77de0cb742df860e0a05cc17c2b592470569
anchoanhelo <anchoanhelo@gmail.com>
2015-03-23 00:03:25 +0100

    -?

--- Szkeleton/src/Game.java
+++ Szkeleton/src/Game.java
@@ -179,7 +179,7 @@ public class Game extends JComponent implements Runnable {
 	public void Start(){
 		SkeletonHelper.writeOutMethodName();
 		
-		if(!SkeletonHelper.getBooleanAnswer("Fut már egy verseny?"))
+		if(!SkeletonHelper.getBooleanAnswer("Fut már egy verseny"))
 			this.run();
 		
 		SkeletonHelper.returnFromMethod();
