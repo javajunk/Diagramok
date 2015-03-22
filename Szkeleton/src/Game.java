c25a9a2f960c60d6395bc44a373ccff9cbec1757
anchoanhelo <anchoanhelo@gmail.com>
2015-03-23 00:32:30 +0100

    Utolsó simítások

--- Szkeleton/src/Game.java
+++ Szkeleton/src/Game.java
@@ -4,6 +4,11 @@ import java.awt.Graphics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Ez az objektum felelős a létrehozási, fő kirajzolási és frissítési metódusok meghívásáért, 
+ * vezeti a játék időkezelését és figyeli, hogy életben vannak-e még a játékosok által vezérelt robotok.
+ */
+
 public class Game extends JComponent implements Runnable {
 
 	private static final long serialVersionUID = 7845653460750690226L;
@@ -175,7 +180,7 @@ public class Game extends JComponent implements Runnable {
 	public void Start(){
 		SkeletonHelper.writeOutMethodName();
 		
-		if(!SkeletonHelper.getBooleanAnswer("Fut már egy verseny?"))
+		if(!SkeletonHelper.getBooleanAnswer("Fut már egy verseny"))
 			this.run();
 		
 		SkeletonHelper.returnFromMethod();
--- Szkeleton/src/Program.java
+++ Szkeleton/src/Program.java
@@ -1,7 +1,6 @@
 /**
- * @author Szendi Tam�s
- * @version 1.0
- * @created 19-m�rc.-2015 11:07:42
+ * A program main metódusát tartalmazó osztály.
+ * Feladata a teljes program indítása.
  */
 
 public class Program {
