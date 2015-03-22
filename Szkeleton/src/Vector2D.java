77c79a94b5722a3c13d11a81ea104f8b3a56f396
Pohubi Zoltán <pohubi.zoltan@gmail.com>
2015-03-22 18:24:58 +0100

    Vector2D kommentek

--- Szkeleton/src/Vector2D.java
+++ Szkeleton/src/Vector2D.java
@@ -4,27 +4,34 @@ public class Vector2D {
 	private double y;
 
 	/**
-	 * 
-	 * @param vec
+	 * Vektorosszeadas.
+	 * @param vec - A hozzaadando vektor.
 	 */
 	public void Add(Vector2D vec){
 		SkeletonHelper.writeOutMethodName();
 		SkeletonHelper.returnFromMethod();
 	}
 
+	/**
+	 * Visszaadja a vektor jelenlegi hosszat.
+	 * @return - A vektor hossza.
+	 */
 	public double Length(){
 		SkeletonHelper.writeOutMethodName();
 		SkeletonHelper.returnFromMethod();
 		return 0;
 	}
 
+	/**
+	 * Normalizalja a vektort.
+	 */
 	public void Normalize(){
 		SkeletonHelper.writeOutMethodName();
 		SkeletonHelper.returnFromMethod();
 	}
 
 	/**
-	 * 
+	 * Skalarszorzas. A parameterkent kapot skalarral megszorozza a vektort
 	 * @param s
 	 */
 	public void Scale(double s){
@@ -33,8 +40,8 @@ public class Vector2D {
 	}
 
 	/**
-	 * 
-	 * @param newVal
+	 * Vektorkivonas.
+	 * @param vec - A kivonando vektor.
 	 */
 	public void Subtract(Vector2D vec){
 		SkeletonHelper.writeOutMethodName();
