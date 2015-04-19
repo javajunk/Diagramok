25d0a23ee8735cbc22a90784c6690a816de769ca
anchoanhelo <anchoanhelo@gmail.com>
2015-04-19 18:44:04 +0200

    t

--- Prototype/8.2.11_RobotLandOnGlue.bat
+++ Prototype/8.2.11_RobotLandOnGlue.bat
@@ -1,4 +1,4 @@
 @echo off
-echo 8.2.11 ControlRobot
-java -classpath "%~dp0\PrototypeTester\bin" Program "java -classpath %~dp0Prototype\bin Program" %~dp0TestFiles\ControlRobot_in.txt %~dp0TestFiles\ControlRobot_out.txt
+echo 8.2.11 RobotLandOnGlue
+java -classpath "%~dp0\PrototypeTester\bin" Program "java -classpath %~dp0Prototype\bin Program" %~dp0TestFiles\RobotLandOnGlue_in.txt %~dp0TestFiles\RobotLandOnGlue_out.txt
 pause
