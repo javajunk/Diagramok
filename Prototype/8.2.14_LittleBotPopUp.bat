35e9e20d8bd13594210c45841dbffd3b4b8ae734
anchoanhelo <anchoanhelo@gmail.com>
2015-04-19 16:39:31 +0200

    Prototipus -> Prototype

---
+++ Prototype/8.2.14_LittleBotPopUp.bat
@@ -0,0 +1,4 @@
+@echo off
+echo 8.2.14 LittleBotPopUp
+java -classpath "%~dp0\PrototypeTester\bin" Program "java -classpath %~dp0Prototype\bin Program" %~dp0TestFiles\LittleBotPopUp_in.txt %~dp0TestFiles\LittleBotPopUp_out.txt
+pause
