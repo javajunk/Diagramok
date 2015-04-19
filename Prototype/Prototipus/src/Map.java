cdf9ce4218b27b803bf647e82d9d12dcd351336d
anchoanhelo <anchoanhelo@gmail.com>
2015-04-19 14:10:39 +0200

    Map 1.0

--- Prototype/Prototipus/src/Map.java
+++ Prototype/Prototipus/src/Map.java
@@ -1,13 +1,58 @@
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import javax.imageio.ImageIO;
 /**
- * Tárolja a pálya képét és a pályát meghatározó maskot. (Szkeletonban nem)
- * Utóbbi segítségével állapítja meg, hogy az adott pozíció a pálya területén található-e.
+ * T�rolja a p�lya k�p�s �s a p�ly�t meghat�roz� maskot.
+ * Ut�bbi seg�ts�g�vel �llap�tja meg, hogy az adott poz�ci� a p�lya ter�let�n tal�lhat�-e.
  */
 public class Map implements GameObject {
 
+	private int protoID;
+	private static int protoIDNext=0;
+	
+	private BufferedImage mapScenery=null; //A térképet alkotó maszkon kívüli grafika
+	private BufferedImage mask=null; //A térkép pályát reprezentáló koordinátáit tartalmazó maszk
+	private BufferedImage trackLine=null;
+	
+	public String mapImage=null;
+	public String outOfTrackMaskFile=null;
+
+	/**
+	 * A Map oszt�ly param�ter n�lk�li konstruktora.
+	 * Ez a konstruktor a relat�v "map.png" �s "mask.png" �tvonalakr�l t�lti be a f�jlokat
+	 */
+
 	//Kellene bele valami amivel le lehet kerdezni a terkep meretet.
 	public Map(){
-
+		this("map.png","mask.png","trackline.png");
+	}
+	
+	/**
+	 * Ez a paraméterezett konstruktor segíti betölteni az adott grafikákat a pályához.
+	 * Ezzel a módszerrel nem kell minden Draw call-nál újra beolvasni a térképet
+	 * @param mappath: a térkép (kiegészítő) grafikáját tartalmazó kép útvonala
+	 * @param maskpath: a térkép maszkját tartalmazó kép útvonala
+	  * @param tracklinepath: a kisrobotok trackline-jának maszkját tartalmazó kép útvonala
+	 */
+	public Map(String mappath,String maskpath, String tracklinepath){
+		protoIDNext++;
+		protoID=protoIDNext;
+		try {
+		    mapScenery = ImageIO.read(new File(mappath));
+		    mask = ImageIO.read(new File(maskpath));
+		    trackLine=ImageIO.read(new File(tracklinepath));
+		    
+		    mapImage=mappath;
+		    outOfTrackMaskFile=mappath;
+		  
+		} catch (IOException e) {
+			
+		}
+		
 	}
 
 	/**
@@ -16,7 +61,7 @@ public class Map implements GameObject {
 	 */
 	public void CollisionWithRobot(Robot robi){
 		boolean onGround = robi.onTheGround();
-		boolean out = isOutOfTrack(new Vector2D());
+		boolean out = isOutOfTrack(robi.getPosition());
 		if(onGround && out)
 		{
 			robi.KillHim();
@@ -29,8 +74,17 @@ public class Map implements GameObject {
 	 * @param pos: vizsgálandó pont
 	 */
 	public boolean isOutOfTrack(Vector2D pos){
-		return false;
+		int X=(int)pos.getX();
+		int Y=(int)pos.getY();
+		int A=mask.getRGB(X, Y) >> 24 & 0xFF;
+		if(A==0){return true;}
+		else{return false;}
 	}
+
+	/*public Vector2D[] getTrackLine(){
+	
+	
+	}*/
 
 	/**
 	 * Frissíti a térkép állapotát.
@@ -39,7 +93,7 @@ public class Map implements GameObject {
 	 */
 	@Override
 	public void Update(Game g){
-
+		
 	}
 	
 	/**
@@ -49,7 +103,19 @@ public class Map implements GameObject {
 	 */
 	@Override
 	public void Draw(Graphics g) {
-
+	
 	}
+	
+	@Override
+	public int getProtoId() {
+		return protoID;
+	}
+
+	@Override
+	public LinkedHashMap<String, String> dump() {
+		LinkedHashMap<String,String> result=new LinkedHashMap<String,String>(){{put("protoID",String.valueOf(protoID));}};
+		return result;
+	}
+
 
 }
